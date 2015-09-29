cf71070604f15bc217d4cdfd1d021ce06ac7fff8
Thorsten Lorenz <thlorenz@gmx.de>
2015-09-29 17:17:26 +0200

    fixing brew content domain

--- bin/docmac.sh
+++ bin/docmac.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-(command -v brew >/dev/null 2>&1) || ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+(command -v brew >/dev/null 2>&1) || ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 brew update
 
